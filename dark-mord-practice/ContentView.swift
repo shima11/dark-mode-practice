59db0eda09a08a384d738b70487aab1342c615a8
Shima <shima.jin@icloud.com>
2019-06-04 23:32:17 -0700

    Initial Commit

---
+++ dark-mord-practice/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  dark-mord-practice
+//
+//  Created by jinsei_shima on 2019/06/04.
+//  Copyright © 2019 jinsei_shima. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
