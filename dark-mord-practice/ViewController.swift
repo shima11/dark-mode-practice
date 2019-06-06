ac5cc2bfc5859ab19b76a3a55c70206ea442c28b
Shima <shima.jin@icloud.com>
2019-06-05 19:12:16 -0700

    wip

---
+++ dark-mord-practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dark-mord-practice
+//
+//  Created by jinsei_shima on 2019/06/05.
+//  Copyright © 2019 jinsei_shima. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewController: UIViewController {
+    
+//    override var overrideUserInterfaceStyle: UIUserInterfaceStyle {
+//        return .dark
+//    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+}
